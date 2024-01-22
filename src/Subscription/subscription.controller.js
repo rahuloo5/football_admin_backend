71d29698444a20292c6f45e4c5a642db5540d656
unknown <vanikmangal7@gmail.com>
2024-01-22 15:01:00 +0530

    changes are done

--- src/Subscription/subscription.controller.js
+++ src/Subscription/subscription.controller.js
@@ -2,20 +2,28 @@ const mongoose = require("mongoose");
 const Subscription = require("../../db/config/subscription.model");
 
 // Create a new subscription
+// const createsubscription = async (req, res) => {
+//   try {
+//     const subscription = new Subscription(req.body);
+//     console.log("subscription plan ", req.body);
+//     await subscription.save();
+//     res.status(201).json(subscription);
+//   } catch (error) {
+//     console.error("Error creating subscription:", error);
+//     res
+//       .status(500)
+//       .json({ error: "Internal Server Error", details: error.message });
+//   }
+// };
+
 const createsubscription = async (req, res) => {
   try {
-    const subscription = new Subscription(req.body);
-    console.log("subscription plan ", req.body);
-    await subscription.save();
-    res.status(201).json(subscription);
+    const subscriptions = await Subscription.find().populate("User");
+    res.json(subscriptions);
   } catch (error) {
-    console.error("Error creating subscription:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error", details: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
-
 // Get all subscriptions
 
 const getAllSubscriptions = async (req, res) => {
