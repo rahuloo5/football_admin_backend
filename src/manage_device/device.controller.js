71922098e4c690867dafe2998a1f7e24f92702b3
mangal vanik <vanikmangal7@gmail.com>
2024-02-09 21:00:08 +0530

    chnages

--- src/manage_device/device.controller.js
+++ src/manage_device/device.controller.js
@@ -2,6 +2,61 @@ const mongoose = require("mongoose");
 const Category = require("../../db/config/categories.model");
 const Device = require("../../db/config/device.model");
 
+// const createDevice = async (req, res) => {
+//   try {
+//     const {
+//       device_name,
+//       description,
+//       privacy_overview: { title1, description1 },
+//       secuirty_overview: { title2, description2 },
+//       other_information: { title3, description3 },
+//       terms_conditions: { title4, description4 },
+
+//       categoryId,
+//       video_url1,
+//       policy_url1,
+//     } = req.body;
+
+//     if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+//       return res
+//         .status(400)
+//         .json({ success: false, error: "Invalid category or subcategory ID" });
+//     }
+
+//     // Fetch the category
+//     const category = await Category.findById(categoryId);
+
+//     if (!category) {
+//       return res
+//         .status(404)
+//         .json({ success: false, error: "Category or subcategory not found" });
+//     }
+
+//     const device = new Device({
+//       device_name,
+//       deviceImages: req.files["deviceImages"]?.map((file) => file.filename),
+//       deviceIcons: req.files["deviceIcons"]?.[0].filename,
+//       video_url: req.files["video_url"]?.[0].filename,
+//       policy_url: req.files["policy_url"]?.[0].filename,
+//       privacy_overview: { title1, description1 },
+//       secuirty_overview: { title2, description2 },
+//       other_information: { title3, description3 },
+//       terms_conditions: { title4, description4 },
+//       description,
+//       video_url1,
+//       policy_url1,
+//       categorie: categoryId,
+//     });
+
+//     await device.save();
+//     res.status(200).json(device);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// };
+
+// / Get all devices
 const createDevice = async (req, res) => {
   try {
     const {
@@ -56,11 +111,13 @@ const createDevice = async (req, res) => {
   }
 };
 
-// / Get all devices
-
 const getAllDevices = async (req, res) => {
   try {
-    const devices = await Device.find();
+    const categoryId = req.query.categoryId;
+
+    const query = categoryId ? { categorie: categoryId } : {};
+
+    const devices = await Device.find(query).populate("categorie");
     res.status(200).json(devices);
   } catch (error) {
     console.error(error);
