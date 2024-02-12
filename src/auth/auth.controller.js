2c7e38479bd5bf8d09963380be532cd3edbacdc1
mangal vanik <vanikmangal7@gmail.com>
2024-02-12 10:16:42 +0530

    changes

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -82,75 +82,6 @@ const verifyOTP = async (req, res) => {
   }
 };
 
-// const verifyOTP = async (req, res) => {
-//   const { number, otp: enteredOTP } = req.body;
-
-//   try {
-//     if (parseInt(enteredOTP) === 1234) {
-//       const user = await User.findOne({ number });
-
-//       if (!user) {
-//         return res.status(404).send({ error: "User not found" });
-//       }
-
-//       // Check if the user is active
-//       const isActive = user.isActive;
-
-//       // Update isActive to true
-//       if (!isActive) {
-//         await User.updateOne({ _id: user._id }, { $set: { isActive: true } });
-//       }
-
-//       const token = GeneratesSignature({
-//         id: user._id,
-//       });
-
-//       return res.status(200).send({
-//         success: true,
-//         message: "Verification code verified successfully",
-//         id: user._id,
-//         isActive: true, // Since it's updated, set it to true
-//         token,
-//       });
-//     } else {
-//       return res.status(400).send({
-//         success: false,
-//         message: "Invalid OTP",
-//       });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ success: false, error: "Internal Server Error" });
-//   }
-// };
-
-// const verify_update = async (req, res) => {
-//   try {
-//     const { number, firstName, lastName, email } = req.body;
-
-//     const user = await User.findOne({ number });
-
-//     if (!user) {
-//       return res.status(404).json({ error: "User not found" });
-//     }
-
-//     user.isActive = true;
-//     user.firstName = firstName;
-//     user.lastName = lastName;
-//     user.email = email;
-
-//     await user.save();
-
-//     return res.status(200).json({
-//       message: "User details updated successfully",
-//       isActive: user.isActive,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
 const verify_update = async (req, res) => {
   try {
     const { number, firstName, lastName, email } = req.body;
@@ -196,6 +127,7 @@ const resendOtp = async (req, res) => {
 
     res.status(200).send({
       message: "Verification code sent successfully",
+      savedOtp,
     });
   } else {
     res.status(404).send("User not found");
--- src/utility/send-sms.js
+++ src/utility/send-sms.js
@@ -1,24 +1,26 @@
-// const twilio = require("twilio");
+const twilio = require("twilio");
 
-// const accountSid = "AC53acf0c303c557b13a2999a68af36f9a";
-// const authToken = "6e2e2ca9875616f61346c7fa6ed06adf";
-// const client = new twilio(accountSid, authToken);
+const accountSid = process.env.TWILLIO_ACCOUNT_SID;
+const authToken = process.env.TWILLIO_AUTH_TOKEN;
+const number = process.env.TWILLIO_PHONE_NUMBER;
 
-// const sendSMS = async (to, body) => {
-//   try {
-//     const message = await client.messages.create({
-//       body: body,
-//       from: "+919074313441",
-//       to: "+916260483976",
-//     });
+const client = new twilio(accountSid, authToken);
 
-//     console.log(`SMS sent successfully. SID: ${message.sid}`);
-//   } catch (error) {
-//     console.error("Error sending SMS:", error.message);
-//   }
-// };
+const sendSMS = async (to, body) => {
+  try {
+    const message = await client.messages.create({
+      body: body,
+      from: number,
+      to: to,
+    });
 
-// // Example usage
-// sendSMS("+919074313441", "Hello, this is a Twilio SMS!");
+    console.log(`SMS sent successfully. SID: ${message.sid}`);
+  } catch (error) {
+    console.error("Error sending SMS:", error.message);
+  }
+};
 
-// module.exports = sendSMS;
+// Example usage
+sendSMS("+919074313441", "Hello, this is a Twilio SMS!");
+
+module.exports = sendSMS;
