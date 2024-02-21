056118ac6ef0f8a7c2438a97b97ecd5dda5e0d37
mangal vanik <vanikmangal7@gmail.com>
2024-02-21 16:19:04 +0530

    error

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const cors = require("cors");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
